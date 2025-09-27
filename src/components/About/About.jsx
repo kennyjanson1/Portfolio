ddd1917c4561f5d4f371b66bb3b64036ba29eef0
kennyjanson1 <kennyjanson1@gmail.com>
2025-09-27 21:10:44 +0700

    first commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import Tilt from 'react-parallax-tilt';
+import profileImage from '../../assets/profile2.jpg';
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="scroll-mt-24 py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-4 md:mt-8 lg:mt-12"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* Left Side */}
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/* Greeting */}
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+          </h1>
+          {/* Name */}
+          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+            Kenny Janson
+          </h2>
+          {/* Skills Heading with Typing Effect */}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white">I am a </span>
+            <ReactTypingEffect
+              text={[
+                'AI/ML Enthusiast',
+                'Passionate Coder',
+                'Curious Coder',
+                'Data Science Learner',
+                'Web Developer'
+              ]}
+              speed={100}
+              eraseSpeed={50}
+              typingDelay={500}
+              eraseDelay={2000}
+              cursorRenderer={(cursor) => (
+                <span className="text-[#8245ec]">{cursor}</span>
+              )}
+            />
+          </h3>
+          {/* About Me Paragraph */}
+          <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
+            Computer Science student with a strong interest 
+            in Artificial Intelligence and Machine Learning. AI has always 
+            inspired me because of its power to shape the future, and 
+            I’m motivated to keep improving my skills in this field.
+          </p>
+          {/* Resume Button */}
+            <div className="flex flex-wrap gap-4 mt-5">
+              {/* Resume Button */}
+              <a
+                href="https://drive.google.com/file/d/15RQ7zbuxL2HDxAYNB-ena3M618crOOvt/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block text-white py-3 px-8 rounded-full text-lg font-bold transition duration-300 transform hover:scale-105"
+                style={{
+                  background: "linear-gradient(90deg, #8245ec, #a855f7)",
+                  boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
+                }}
+              >
+                Download CV
+              </a>
+
+              {/* Contact Button (scroll ke section contact) */}
+              <a
+                href="#contact"
+                className="inline-block py-3 px-8 rounded-full text-lg font-bold border transition duration-300 transform hover:scale-105"
+                style={{
+                  color: "white", // teks ungu solid
+                  borderColor: "#8245ec",
+                }}
+              >
+                Contact Me
+              </a>
+            </div>
+        </div>
+        {/* Right Side */}
+        <div className="md:w-1/2 flex justify-center md:justify-end">
+          <Tilt
+            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
+            tiltMaxAngleX={20}
+            tiltMaxAngleY={20}
+            perspective={1000}
+            scale={1.05}
+            transitionSpeed={1000}
+            gyroscope={true}
+          >
+            <img
+              src={profileImage}
+              alt="Tarun Kaushik"
+              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+            />
+          </Tilt>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/constants.js
@@ -0,0 +1,229 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import flask from './assets/tech_logo/flask.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logo's
+// import githubdetLogo from './assets/work_logo/github_det.png';
+// import csprepLogo from './assets/work_logo/cs_prep.png';
+// import movierecLogo from './assets/work_logo/movie_rec.png';
+// import taskremLogo from './assets/work_logo/task_rem.png';
+// import npmLogo from './assets/work_logo/npm.png';
+// import webverLogo from './assets/work_logo/web_dig.png';
+// import cmLogo from './assets/work_logo/cm.png';
+// import imagesearchLogo from './assets/work_logo/image_search.png';
+// import removebgLogo from './assets/work_logo/remove_bg.png';
+import havenLogo from './assets/work_logo/haven.png';
+import faceLogo from './assets/work_logo/faceme.png';
+import cateringzLogo from './assets/work_logo/cateringz.png';
+import languageClassifierLogo from './assets/work_logo/languageclassifier.png';
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'Flask', logo: flask },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  // export const experiences = [
+  //   {
+  //     id: 0,
+  //     img: webverseLogo,
+  //     role: "Fullstack Developer",
+  //     company: "Webverse Digital",
+  //     date: "April 2024 - Present",
+  //     desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+  //     skills: [
+  //       "HTML",
+  //       "CSS",
+  //       "JavaScript",
+  //       "React JS",
+  //       "TypeScript",
+  //       "Node JS",
+  //       "Tailwind CSS",
+  //       "MongoDb",
+  //       "Redux",
+  //       " Next Js",
+  //     ],
+  //   },
+  //   {
+  //     id: 1,
+  //     img: agcLogo,
+  //     role: "Fullstack Engineer",
+  //     company: "Agumentik Group of Companies",
+  //     date: "July 2023 - March 2024",
+  //     desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+  //     skills: [
+  //       "ReactJS",
+  //       "Redux",
+  //       "JavaScript",
+  //       "Tailwind CSS",
+  //       "HTML",
+  //       "CSS",
+  //       "SQL",
+  //     ],
+  //   },
+  //   {
+  //     id: 2,
+  //     img: newtonschoolLogo,
+  //     role: "Frontend Intern",
+  //     company: "Newton School",
+  //     date: "September 2021 - August 2022",
+  //     desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+  //     skills: [
+  //       "HTML",
+  //       "CSS",
+  //       "Javascript",
+  //       "Bootstrap",
+  //       "Figma",
+  //       "Material UI",
+  //     ],
+  //   },
+  // ];
+  
+  // export const education = [
+  //   {
+  //     id: 0,
+  //     img: glaLogo,
+  //     school: "GLA University, Mathura",
+  //     date: "Sept 2022 - July 2024",
+  //     grade: "7.81 CGPA",
+  //     desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
+  //     degree: "Master of Computer Applications - MCA",
+  //   },
+  //   {
+  //     id: 1,
+  //     img: bsaLogo,
+  //     school: "BSA College, Mathura",
+  //     date: "Sept 2018 - Aug 2021",
+  //     grade: "73.2%",
+  //     desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+  //     degree: "Bachelor of Science - BSC (Computer Science)",
+  //   },
+  //   {
+  //     id: 2,
+  //     img: vpsLogo,
+  //     school: "Vatsalya Public School Govardhan, Mathura",
+  //     date: "Apr 2017 - March 2018",
+  //     grade: "78%",
+  //     desc: "I completed my class 12 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+  //     degree: "CBSE(XII) - PCM with Computer Science",
+  //   },
+  //   {
+  //     id: 3,
+  //     img: vpsLogo,
+  //     school: "Vatsalya Public School Govardhan, Mathura",
+  //     date: "Apr 2015 - March 2016",
+  //     grade: "87.5%",
+  //     desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
+  //     degree: "CBSE(X), Science with Computer Application",
+  //   },
+  // ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: " CAteriNgz",
+      description:
+        "CAteriNgz is a web-based catering order platform prototype developed to simulate the user experience of browsing and placing catering orders online.",
+      image: cateringzLogo,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/kennyjanson1/CAteriNgz.git",
+      webapp: "https://drive.google.com/file/d/1pl7nvYrXIMUCKJZafdtGdCFjAEaW6nIc/view?usp=sharing",
+    },
+    {
+      id: 1,
+      title: "FaceMe",
+      description:
+        "A group project that detects whether an input image is real or AI-generated using deep learning, aimed at preventing the spread of fake news.",
+      image: faceLogo,
+      tags: ["React JS", "JavaScript", "Flask" , "Python"],
+      github: "https://github.com/kennyjanson1/AI-Image-Classification.git",
+      webapp: "https://drive.google.com/file/d/1ieO7R0cIeysMPaehTtjgKneKdwtRJvlE/view?usp=sharing",
+    },
+    {
+      id: 2,
+      title: "Haven",
+      description:
+        " A web-based AI project that predicts users MBTI personality types through natural language input, aiming to make personality prediction simple, accurate, and meaningful.",
+      image: havenLogo,
+      tags: ["React JS", "JavaScript", "Flask" , "Python"],
+      github: "https://github.com/kennyjanson1/MBTI-Personality-Tracking.git",
+      webapp: "Not Available",
+    },
+    {
+      id: 3,
+      title: "Language Classifier ",
+      description:
+        " Language Classifier is a web app that detects the language of user-input text using an XLM-RoBERTa transformer model, built with React (frontend) and Flask (backend)",
+      image: languageClassifierLogo,
+      tags: ["React JS", "Node.js", "NPM", "Validation"],
+      github: "https://github.com/kennyjanson1/Language-Detection.git",
+      webapp: "https://drive.google.com/file/d/1Gdo-t5NF6YnOpgGsiQdXxIocx9up6lmI/view?usp=sharing",
+    },
+  ];  
